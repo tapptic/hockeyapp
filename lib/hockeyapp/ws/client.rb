34ba4d4b8fdec4cb59538c8ae09de3f1a5b6ee79
Paul Renson <paulrenson4@gmail.com>
2012-05-30 10:13:11 +0200

    uncache apps and versions

--- lib/hockeyapp/ws/client.rb
+++ lib/hockeyapp/ws/client.rb
@@ -6,11 +6,9 @@ module HockeyApp
     end
 
     def get_apps
-      Rails.cache.fetch("apps", :expires_in => 1.hour) do
         apps_hash = ws.get_apps
         assert_success apps_hash
         apps_hash["apps"].map{|app_hash|App.from_hash(app_hash, self)}
-      end
     end
 
     def get_crashes app
@@ -34,14 +32,11 @@ module HockeyApp
     end
 
     def get_versions app
-      Rails.cache.fetch("versions/#{app.public_identifier}", :expires_in => 1.hour) do
-        versions_hash = ws.get_versions app.public_identifier
-        versions_hash["app_versions"].map{|version_hash|Version.from_hash(version_hash, app, self)}
-      end
+      versions_hash = ws.get_versions app.public_identifier
+      versions_hash["app_versions"].map{|version_hash|Version.from_hash(version_hash, app, self)}
     end
 
     def post_new_version version
-      Rails.cache.delete("versions/#{version.app.public_identifier}")
       app_id = version.app.public_identifier
       ipa = version.ipa
       raise "There must be an executable file" if ipa.nil?
@@ -51,14 +46,12 @@ module HockeyApp
     end
 
     def remove_app app
-      Rails.cache.delete("apps")
       resp = ws.remove_app app.public_identifier
       raise "unexpected response" if resp.code != 200
       resp.code == 200
     end
 
     def create_app file_ipa
-      Rails.cache.delete("apps")
       resp = ws.post_new_app(file_ipa)
       raise resp['errors'].map{|e|e.to_s}.join("\n") unless resp['errors'].nil?
       App.from_hash(resp, self)
