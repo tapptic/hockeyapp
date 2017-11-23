825572ebd03d20aee744a7d4c5c2144969ed74ac
Martin Kim Dung-Pham <kim@elbedev.com>
2017-11-23 11:24:20 +0100

    Linter changes

--- lib/hockeyapp.rb
+++ lib/hockeyapp.rb
@@ -4,6 +4,7 @@ require 'hockeyapp/models/active_model_compliance'
 require "hockeyapp/models/app"
 require 'hockeyapp/models/crash'
 require 'hockeyapp/models/version'
+require 'hockeyapp/models/statistic'
 require 'hockeyapp/models/crash_group'
 require 'hockeyapp/ws/ws'
 require 'hockeyapp/ws/client'
